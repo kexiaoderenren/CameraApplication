9a1219270be6a63e53a7dbe30cdceb7ed35e52b5
gaokuncheng <2450600200@qq.com>
2017-06-12 16:45:39 +0800

    add camera2 application demo

--- app/src/main/java/com/wine9/cameraapplication/MainActivity.java
+++ app/src/main/java/com/wine9/cameraapplication/MainActivity.java
@@ -3,11 +3,11 @@ package com.wine9.cameraapplication;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.btn_txt1).setOnClickListener(this);
         findViewById(R.id.btn_txt2).setOnClickListener(this);
+        findViewById(R.id.btn_txt3).setOnClickListener(this);
         int check = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (check == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "权限已经授权！", Toast.LENGTH_SHORT).show();
@@ -42,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_txt2:
                 intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_txt3:
+                intent = new Intent(MainActivity.this, Camera2Activity.class);
                 startActivity(intent);
                 break;
         }
